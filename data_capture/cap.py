29590d7d32e1309a2a93bea6869eb2ff2f15b641
TevinAchong <tevinachong@hotmail.com>
2019-11-03 10:54:02 -0400

    new updates

--- data_capture/cap.py
+++ data_capture/cap.py
@@ -1,7 +1,9 @@
-import sys
 
-if sys.version_info < 3.0:
-    pass
+from textblob import TextBlob
+from textblob.sentiments import NaiveBayesAnalyzer
+
+blob = TextBlob("In 1992 Delta Air Lines had its", analyzer=NaiveBayesAnalyzer())
+print(blob.sentiment)
 
 
 # Retrieving all tweets mentioning <company_name>
