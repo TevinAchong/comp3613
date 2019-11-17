1f6df059066251c047b928d7cf38f60e2afce38f
TevinAchong <tevinachong@hotmail.com>
2019-11-17 10:41:47 -0400

    vader sa script

--- sentiment_analysis/sentiment_analysis.py
+++ sentiment_analysis/sentiment_analysis.py
@@ -1,11 +1,27 @@
-import nltk
-import random
-from nltk.classify.scikitlearn import SklearnClassifier
-import pickle
-from sklearn.naive_bayes import MultinomialNB, BernoulliNB
-from sklearn.linear_model import LogisticRegression, SGDClassifier
-from sklearn.svm import SVC, LinearSVC, NuSVC
-from nltk.classify import ClassifierI
-from statistics import mode
-from nltk.tokenize import word_tokenize
-import re
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+# Globally declaring the analyzer
+analyser = SentimentIntensityAnalyzer()
+
+def tweet_polarity_scores(tweet):
+    '''
+    Input: a tweet in the form of a string
+    Output: polarity scores for that tweet indicating positive, negative, and neutral
+    '''
+    score = analyser.polarity_scores(tweet)
+    compound = score['compound']
+    
+    # The tweet is considered positive
+    if compound >= 0.05:
+        return 1
+    
+    # The tweet is considered neutral
+    elif (compound > -0.05) and (compound < 0.05):
+        return 0
+    
+    # The tweet is considered negative
+    else:
+        return -1
+
+if __name__ == '__main__':
+    print(tweet_polarity_scores("This movie is VERY GOOD!"))
