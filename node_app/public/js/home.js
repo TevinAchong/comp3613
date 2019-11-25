36e1b1415b71c9f3ead69d1be5b027a9b39b7158
TevinAchong <tevinachong@hotmail.com>
2019-11-25 03:35:08 -0400

    still working on load screen

--- node_app/public/js/home.js
+++ node_app/public/js/home.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
     $('.datepicker').datepicker();
   });
+
+function show(id, value) {
+    document.getElementById(id).style.display = value ? 'block' : 'none';
+}
+$(window).on("unload", function() {
+    document.getElementById('loading').visibility = 'visible';
+    document.getElementById('page').visibility = 'hidden';
+});
